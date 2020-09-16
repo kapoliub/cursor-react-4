82d3fc2683f00b1cfadd79c74e4169f8765251bb
Liubomyr <louidropson@gmail.com>
2020-09-16 15:42:39 +0300

    Updates

---
+++ precache-manifest.02e1156e2b0cbeffc69ff056021793ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c8f301570b1edbc3d156084e110c5fc",
+    "url": "/cursor-react-4/index.html"
+  },
+  {
+    "revision": "fb890fae743af3e68b13",
+    "url": "/cursor-react-4/static/css/main.5d186460.chunk.css"
+  },
+  {
+    "revision": "2963bb67b6e949f71b55",
+    "url": "/cursor-react-4/static/js/2.0cb813e8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cursor-react-4/static/js/2.0cb813e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb890fae743af3e68b13",
+    "url": "/cursor-react-4/static/js/main.d14b1b7b.chunk.js"
+  },
+  {
+    "revision": "66c75dce3940aed68773",
+    "url": "/cursor-react-4/static/js/runtime-main.5062c86c.js"
+  },
+  {
+    "revision": "458958ee10ed359032f7d2d95ea9ae65",
+    "url": "/cursor-react-4/static/media/padlock.458958ee.svg"
+  }
+]);
